388c1222505387bfbf6776c14df5bb3fd7df0c6a
firekidu <fcsfunfact@gmail.com>
2025-08-11 23:54:50 +0100

    Update Renderer.tsx

--- apps/web/app/components/Renderer.tsx
+++ apps/web/app/components/Renderer.tsx
@@ -16,7 +16,7 @@ export default function Renderer({ blueprintId, spec }: any) {
         {spec.pages.map((p:any,i:number)=> (
           <div key={i} id={'p'+i} style={{ marginBottom:40 }}>
             <h2>{p.title}</h2>
-            {p.components.map((c:any,j:number) => {
+            {p.components.map((c:any, j:number)=>{
               const fields = spec.collections.find((x:any)=> x.name === c.collection)?.fields || [];
               if (c.type === 'form') return <Form key={j} blueprintId={blueprintId} collection={c.collection} fields={fields} />;
               if (c.type === 'table') return <Table key={j} blueprintId={blueprintId} collection={c.collection} fields={fields} />;
